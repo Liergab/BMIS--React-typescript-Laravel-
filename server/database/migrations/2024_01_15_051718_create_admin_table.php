c075fd55bcc1c5d9c1fa62fa9287265cbcc28036
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-01-31 19:56:41 +0800

    add_role_and token

--- server/database/migrations/2024_01_15_051718_create_admin_table.php
+++ server/database/migrations/2024_01_15_051718_create_admin_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('password');
+            $table->string('role')->default('admin');
             $table->rememberToken();
             $table->timestamps();
         });
