e593132fe18e8598666a56cb9c5444d2b4e8adfd
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-02-13 13:19:09 +0800

    Add_Logic_Methos_for_Api

--- server/app/Http/Controllers/Api/AdminController.php
+++ server/app/Http/Controllers/Api/AdminController.php
@@ -19,4 +19,48 @@ class AdminController extends Controller
 
         return response()->json(['message' => 'Resident status updated successfully']);
     }
+
+    public function getResidentById($residentId)
+    {
+        $resident = Resident::find($residentId);
+
+        if($resident){
+            return response()->json([
+                $resident
+            ],200);
+        }else{
+            return response()->json([
+                'error' => 'No resident found'
+            ],404);
+        }
+    }
+
+    public function getAllResidentPending(){
+        $resident = Resident::where('status','pending')->get();
+
+        return response()->json([
+            $resident
+        ],200);
+    }
+
+    public function getResidentApproved()
+    {
+        $resident = Resident::where('status', 'approved')->get();
+    
+            return response()->json([
+                $resident
+            ], 200);
+      
+    }
+
+    public function getResidentDisapproved()
+    {
+        $resident = Resident::where('status', 'disapproved')->get();
+    
+            return response()->json([
+                $resident
+            ], 200);
+      
+    }
+
 }
