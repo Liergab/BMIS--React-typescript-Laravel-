fb26da02648f8debcd0c3933aa0ed69d63eee54d
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-02-15 14:22:14 +0800

    add_real_data_coming_from_backend

--- client/src/components/AdminComponent/ResidentTable.tsx
+++ client/src/components/AdminComponent/ResidentTable.tsx
@@ -1,66 +1,133 @@
-import { Button, Typography } from '@mui/material';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-import React from 'react'
+import View from './modal/View';
+import { DataGrid,
+         GridToolbar }    from '@mui/x-data-grid';
+import { GridCellParams } from '@mui/x-data-grid';
 
-const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
+import { Button,
+         CircularProgress, 
+         Typography,
+         Box     }          from '@mui/material';
+
+import React, { useState }  from 'react';
+import EditIcon             from '@mui/icons-material/Edit';
+import VisibilityIcon       from '@mui/icons-material/Visibility';
+import PersonAddIcon        from '@mui/icons-material/PersonAdd';
+import EditResident         from './modal/EditResident';
+import { useSelector }      from 'react-redux';
+import { RootState }        from '../../service/state/store';
+import { FetchResidentApproved} from '../../service/api/AdminQuery';
+import { ResidentTable as Resident } from '../../types';
+
+const VISIBLE_FIELDS = ['id', 'first_name', 'last_name', 'email'];
+
+
+
+const ResidentTable = () => {
+  //fetch token from redux
+  const {adminInfo} = useSelector((state:RootState) => state.admin)
+  const token = adminInfo?.token;
+  // View state
+  const [openView,setOpenView] = useState<boolean>(false)
+  const [idView, setIdView] = useState<number | null>(null)
+
+  // Edit state
+  const [openEdit, setOpenEdit] = useState<boolean>(false)
+  const [idEdit, setIdEdit] = useState<number | null>(null)
+
+  // query for  fetching datd
+  const {data, isLoading} = FetchResidentApproved({token})
+
+  if(isLoading) return <CircularProgress />
+  const rows = (Object.values(data)[0] as Resident[])?.map((p:Resident) => ({
+    id: p?.id,
+    first_name: p?.first_name,
+    last_name: p?.last_name,
+    email :p?.email,
+  }));
+
+  const columns = [
+    ...VISIBLE_FIELDS.map((field) => ({
+      field,
+      headerName: field.charAt(0).toUpperCase() + field.slice(1),
+      width: 150,
+    })),
     {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 90,
-    },
-    {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params: GridValueGetterParams) =>
-        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+      field: 'action',
+      headerName: 'Action',
+      width: 180,
+      renderCell: (params: GridCellParams) => (
+        <div className='flex gap-1'>
+        <Button
+          variant="outlined"
+          color="success"
+          size="small"
+          startIcon={<EditIcon />}
+          onClick={() => handleEdit(params.row.id)}
+        >
+          Edit
+        </Button>
+        <Button
+          variant="outlined"
+          color="success"
+          size="small"
+          startIcon={<VisibilityIcon />}
+          onClick={() => handleView(params.row.id)}
+        >
+          View
+        </Button>
+        </div>
+        
+      ),
     },
   ];
-  
-  const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  ];
-  
 
-const ResidentTable:React.FC = () => {
+  const handleView = (id:number) => {
+    setOpenView((prev) => !prev)
+    setIdView(id)
+  }
+
+  const handleEdit = (id:number) => {
+    setOpenEdit((prev) => !prev)
+    setIdEdit(id)
+  }
+
   return (
-  <>
+    <React.Fragment>
       <div className='flex items-center justify-between w-full'>
-          <Typography variant='h5' className='text-gray-dark' sx={{fontWeight:'600'}}>
-              Resident Table
-          </Typography>
-          <Button variant="contained" ><span className=' font-medium'>Add</span></Button>
+      
+      <Typography variant='h5' className='text-gray-dark' sx={{fontWeight:'600'}}>
+          Resident Table
+      </Typography>
+        <Button 
+          variant="outlined" 
+          color='primary' 
+          startIcon={<PersonAddIcon/>} >
+            <span className=' font-medium'>
+              Add
+            </span>
+        </Button>
+      
       </div>
-      <div className='bg-text rounded-lg drop-shadow-2xl' style={{ height: 400, width: '100%' }}>
+      <Box sx={{  width: 1 }} className='bg-text  h-[400px]  md:h-[500px] drop-shadow-xl p-4 border-0 rounded-lg'>
         <DataGrid
-          rows={rows}
-          columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: { page: 0, pageSize: 5 },
+        rows={rows}
+        columns={columns}
+          disableColumnFilter
+          disableColumnSelector
+          disableDensitySelector
+        
+          slots={{ toolbar: GridToolbar }}
+          slotProps={{
+            toolbar: {
+              showQuickFilter: true,
             },
           }}
-          pageSizeOptions={[5, 10]}
-          checkboxSelection
-          className='rounded-lg'
         />
-      </div>
-    </>
-  )
+      </Box>
+      {openView && <View openView={openView} setOpenView={setOpenView} id={idView}/>}
+      {openEdit && <EditResident openEdit={openEdit} setOpenEdit={setOpenEdit} id={idEdit}/>}
+    </React.Fragment>
+  );
 }
 
 export default ResidentTable
