a83352c0a2ce40b53be429e989e88631a0791359
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-02-15 14:35:41 +0800

    Add_providers

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,19 @@
-import React    from 'react'
-import ReactDOM from 'react-dom/client'
-import App      from './App.tsx'
+import React    from 'react';
+import ReactDOM from 'react-dom/client';
+import App      from './App.tsx';
 import './index.css'
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter}      from 'react-router-dom';
+import MyReactQueryProvider from './service/provider/react-query/MyReactQueryProvider.tsx';
+import MyReduxProvider      from './service/provider/redux/MyReduxProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <MyReduxProvider>
+      <MyReactQueryProvider>
+        <BrowserRouter>
+          <App />
+      </BrowserRouter>
+      </MyReactQueryProvider>
+    </MyReduxProvider>
   </React.StrictMode>,
 )
