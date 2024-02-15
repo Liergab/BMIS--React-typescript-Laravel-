bf9ea7214001512c3271b57989aaf0d4ce52d9e3
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-02-15 14:27:55 +0800

    fix_login

--- client/src/components/AdminLoginCom.tsx
+++ client/src/components/AdminLoginCom.tsx
@@ -1,35 +1,85 @@
-import React           from 'react';
+import React, { useEffect }  from 'react';
 import TextFieldUI     from './UI/TextFieldUI';
 import {useForm}       from 'react-hook-form';
 import useToggle       from '../hooks/useToggle';
 import {z}             from 'zod';
+import toast           from 'react-hot-toast';
 import { zodResolver } from '@hookform/resolvers/zod';
-import {
-    Box,
-    Button,
-    Typography }      from '@mui/material';
-import ForgetPassword from './ForgetPassword';
+import {Box,
+        Button,
+        CircularProgress,
+        Typography }   from '@mui/material';
+import ForgetPassword  from './ForgetPassword';
+import { useMutation } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
+import { useAdminLoginApi } from '../service/api/Auth';
+import { AxiosError }     from 'axios';
+import { setCredentials } from '../service/state/slice/authAdminSlice';
+import { useDispatch }    from 'react-redux';
+import { AdminLoginSchema } from '../schemas/FormSchema';
 
-const adminSchema = z.object({
-    email: z.string().email(),
-    password: z
-    .string()
-    .min(8,{ message: 'Invalid Password' })
-    .max(12)
-})
-type formFields = z.infer<typeof adminSchema>
 
+type formFields = z.infer<typeof AdminLoginSchema>
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function isAxiosError(error: any): error is AxiosError {
+    return (error as AxiosError)?.isAxiosError === true;
+  }
 
 const AdminLoginCom:React.FC = () => {
-    const {state, toggleState, setState} = useToggle({initialValue:false})
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const AdminLogin = useMutation({
+        mutationFn: useAdminLoginApi,
+        onSuccess: () => {
+            navigate('/admin/dashboard')
+        }
+    });
+
+    const {state,
+          toggleState, 
+          setState} = useToggle({initialValue:false})
+
     const {register,
-         handleSubmit,
-        formState:{errors, isSubmitting}} = useForm<formFields>({resolver:zodResolver(adminSchema)})
+           reset,
+           handleSubmit,
+           formState,
+            formState:{
+                errors, 
+                isSubmitting, 
+                isSubmitSuccessful}} = useForm<formFields>({
+                    resolver:zodResolver(AdminLoginSchema)
+                })
 
     const onSubmit = async(data:formFields) => {
-       await new Promise((resolve) => setTimeout(resolve,1000));
-       console.log(data)
+        try {
+            const res = await AdminLogin.mutateAsync(data)
+            dispatch(setCredentials(res))
+           } catch (e) {
+            if (isAxiosError(e)) {
+                const errorMessage = (e.response?.data as { message?: string })?.message || 'An error occurred.';
+                toast.error(errorMessage)
+              } else {
+                console.error('Unexpected error:', e);
+            }
+           }
     }
+
+    useEffect(() => {
+        if(formState.isSubmitSuccessful){
+          return reset()
+        }
+    },[formState,isSubmitSuccessful,reset])
+
+    useEffect(() => {
+        if (errors.password) {
+          toast.error(errors.password.message!);
+        }
+        if(errors.email){
+            toast.error(errors.email.message!);
+        }
+      }, [errors.password, errors.email]);
 
   return (
     <>
@@ -56,16 +106,18 @@ const AdminLoginCom:React.FC = () => {
                 label='Admin Email' 
                 type='text'
                 innerRef={register('email')}
+                hasError={!!errors.email} 
             />
-              {errors.email &&  <span className="text-sm   text-bg-button ">{errors.email.message}</span>}
+            
             <TextFieldUI 
                 id='password' 
                 size='small' 
                 label='Admin password' 
                 type='password'
                 innerRef={register('password')}
+                hasError={!!errors.password} 
             />
-             {errors.password &&  <span className="text-sm text-bg-button ">{errors.password.message}</span>}
+             
 
             <div className='flex w-full items-center justify-between'>
                 <Button
@@ -84,19 +136,24 @@ const AdminLoginCom:React.FC = () => {
                         fontSize: '12px',
                     },
                 }}>
-                 {isSubmitting ? "Loading..." : "Login"}
+                 {isSubmitting ?
+                  <span className='flex items-center gap-2 text-text'>
+                    <CircularProgress size={20}/> Loading...
+                  </span>  
+                  : "Login"}
                 </Button>
 
                 <Typography 
-                 sx={{
-                    fontWeight:'600',
-                    color:"#8492a6",
-                      fontSize:'16px',
-                      '@media (max-width: 600px)': {
-                        fontSize: '12px',
-                    },
-                }}
-                onClick={toggleState}
+                    className='cursor-pointer hover:underline'
+                    sx={{
+                        fontWeight:'600',
+                        color:"#8492a6",
+                        fontSize:'16px',
+                        '@media (max-width: 600px)': {
+                            fontSize: '12px',
+                        },
+                    }}
+                    onClick={toggleState}
                 >
                     Forget Password
                 </Typography>
